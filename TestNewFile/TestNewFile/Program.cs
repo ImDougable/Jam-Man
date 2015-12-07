68caf02d357e17c687d78accbda66c1aee08b3ab
Douglas Chesson <douglas.chesson91@gmail.com>
2015-12-06 21:56:30 -0500

    Alright let's do dis.

---
+++ TestNewFile/TestNewFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNewFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a test to seeing if I can
+            //add a file from Visual Studio.
+            string myString = "jibber jabber";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine()
+            }
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
